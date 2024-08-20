93c17a67d1538368b0e2c6e0e4d64b6dcb4ea5bf
TheCrazyHitomi <amandine-tomas@hotmail.fr>
2024-08-20 17:01:30 +0200

    create intro component TS

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Hero from "../assets/components/hero/hero";
+import Intro from "../assets/components/intro/intro";
 
 const Home: React.FC =() =>{
     return(
         <>
             <Hero/>
+            <Intro/>
         </>
     )
 }
---
+++ src/assets/components/intro/intro.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+
+const Intro: React.FC =() =>{
+
+    return (
+        <>
+            <div className="intro">
+
+                <div className="introText">
+                    <h2> blob blob blob</h2>
+                    <h3>kakou kakou kakou kakou kakou kakou kakou kakou kakou kakou kakou kakou kakou kakou kakou kakou kakou kakou  kakou kakou </h3>
+                </div>
+
+                <img className="introPic" src="src/assets/image/clarissa-watson-kWL3Wo5x7Ic-unsplash.jpg" alt="chariots petit train rouge"/>
+            </div>
+        </>
+    )
+}
+
+export default Intro;
