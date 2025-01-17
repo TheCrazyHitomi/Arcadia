16d1a0a2bdde9d1702a35c200e19694058387795
TheCrazyHitomi <amandine-tomas@hotmail.fr>
2025-01-17 15:05:58 +0100

    fix bug background image visit component

--- src/assets/components/visit/visit.tsx
+++ src/assets/components/visit/visit.tsx
@@ -5,18 +5,21 @@ const VisitAnim: React.FC =() =>{
     return(
         <>
             <div className="visitComponent">
+                <img className="visit-background" src="./image/background/leaf3.png"  alt="" />
+                <div className="visit-content">
                 <div className="visitTxt">
                     <h1>Pour une visite <br/> plus dynamique</h1>
                     <h3>découvrez le parc avec notre meilleur accompagnement, adapté a vos attentes.</h3>
                 </div>
-                <div className="visitPics">
-                    <div className="animPics">
-                        <h2 className="animTxt">Visite guidée avec un membre<br/>de notre equipe</h2>
-                        <img className="animPic" src="./image/isaac-chou-CYqh_2Vju1E-unsplash.jpg" alt="" />
-                    </div>
-                    <div className="animPics">
-                        <h2 className="animTxt">Visite à bord<br/>de notre petit train</h2>
-                        <img className="animPic" src="./image/FrontCards/animationPic.jpg"  alt="" />
+                    <div className="visitPics">
+                        <div className="animPics">
+                            <h2 className="animTxt">Visite guidée avec un membre<br/>de notre equipe</h2>
+                            <img className="animPic" src="./image/isaac-chou-CYqh_2Vju1E-unsplash.jpg" alt="" />
+                        </div>
+                        <div className="animPics">
+                            <h2 className="animTxt">Visite à bord<br/>de notre petit train</h2>
+                            <img className="animPic" src="./image/FrontCards/animationPic.jpg"  alt="" />
+                        </div>
                     </div>
                 </div>
             </div>
